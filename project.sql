72bf4cb79ce218c4f66d8e496a7e756505fa1813
Lucas Bailey <baileylu@eos05.cis.gvsu.edu>
2019-12-02 13:39:20 -0500

    debugging table creation

--- project.sql
+++ project.sql
@@ -5,13 +5,28 @@ CIS 353 - Database Design Project
 Lucas Bailey
 Sam Petrarca
 */ 
+SET sqlblanklines ON
+
+DROP TABLE clownCars;
+DROP TABLE clown;
+DROP TABLE clownCourse;
+DROP TABLE clownProfessor;
+DROP TABLE sponsor;
+DROP TABLE award;
+DROP TABLE locations;
+DROP TABLE enrolledIn;
+DROP TABLE teaches;
 
 CREATE TABLE clownCars
 (
 vIN INTEGER PRIMARY KEY,
 make CHAR(15) NOT NULL,
 model CHAR(15) NOT NULL,
-cSSN INTEGER NOT NULL
+cSSN INTEGER NOT NULL,
+
+CONSTRAINT ccIC1 FOREIGN KEY (cSSN) REFERENCES clown(cSSN)
+    ON DELETE CASCADE
+    DEFERRABLE INITIALLY DEFERRED
 );
 
 CREATE TABLE clown
@@ -41,7 +56,7 @@ age INTEGER
 CREATE TABLE sponsor
 (
 sID INTEGER PRIMARY KEY,
-sName CHAR(15) NOT NULL,
+sName CHAR(15) NOT NULL
 );
 
 CREATE TABLE award
@@ -72,7 +87,7 @@ CONSTRAINT cID1 FOREIGN KEY (cID)
     REFERENCES clownCourse(cID)
 );
 
-CREATE TABLE enrolledIN
+CREATE TABLE enrolledIn
 (
 cSSN INTEGER,
 cID INTEGER,
@@ -94,7 +109,7 @@ sem CHAR(10),
 year INTEGER,
 PRIMARY KEY(pSSN, cID),
 
-CONSTRAINT pSSN1 FOREIGN KEY (pSSN)
+CONSTRAINT pSSN1 FOREIGN KEY (pSSN) 
     REFERENCES clownProfessor(pSSN),
 CONSTRAINT cID3 FOREIGN KEY (cID)
     REFERENCCES clownCourse (cID)
@@ -109,21 +124,34 @@ SET FEEDBACK OFF
 INSERT INTO clownCar VALUES (1, 'clowny', 'silverado', 1);
 INSERT INTO clownCar VALUES (2, 'bozo', 'prius', 2);
 INSERT INTO clown VALUES (1, 'Jef', 20);
-INSERT INTO clown VALUES (2, 'Cam', 27);
+INSERT INTO clown VALUES (2, 'Cam', 40);
+INSERT INTO clown VALUES (3, 'Carl', 12);
 INSERT INTO clownCourse VALUES (1, 'Juggling', 3);
 INSERT INTO clownProfessor VALUES (1, 'Jesus!', 35);
 INSERT INTO sponsor VALUES (1, 'macDangles');
-INSERT INTO award VALUES (1, 1, 01/30/2019, 'Participation', 300);
+INSERT INTO award VALUES (1, 1, 2019-01-30, 'Participation', 300);
+INSERT INTO award VALUES (2, 1, 2019-02-10, 'Participation', 900);
 INSERT INTO locations VALUES (1, 'Allendale');
 INSERT INTO enrolledIn VALUES (1, 1, 'Winter', 2020);
 INSERT INTO teaches VALUES (1, 1, 'Winter', 2020);
 
-< The INSERT statements that populate the tables> 
-Important: Keep the number of rows in each table small enough so that the results of your queries can be verified by hand. See the Sailors database as an example. 
+/*< The INSERT statements that populate the tables> 
+Important: Keep the number of rows in each table small enough so that the results of your queries can be verified by hand. See the Sailors database as an example. */
 SET FEEDBACK ON 
 COMMIT; 
 -- 
-< One query (per table) of the form: SELECT * FROM table; in order to print out your database > 
+/*< One query (per table) of the form: SELECT * FROM table; in order to print out your database > */
+
+SELECT * FROM clownCar;
+SELECT * FROM clown;
+SELECT * FROM clownCourse;
+SELECT * FROM clownProfessor;
+SELECT * FROM sponsor;
+SELECT * FROM award;
+SELECT * FROM locations;
+SELECT * FROM enrolledIn;
+SELECT * FROM teaches;
+
 -- 
 < The SQL queries>. Include the following for each query: 
 6. A comment line stating the query number and the feature(s) it demonstrates (e.g. – Q25 – correlated subquery). 
